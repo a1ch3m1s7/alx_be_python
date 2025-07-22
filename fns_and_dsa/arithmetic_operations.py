061983a0f61a7a2b0aa05c057a1d5b507970e31e
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-07-22 14:36:26 +0300

    Add fns_and_dsa

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,23 @@
+def perform_operation(num1, num2, operation):
+	# define arithmetic operations.
+	match operation:
+		case "add":
+			result = num1 + num2
+			return result
+
+		case "subtract":
+			result = num1 - num2
+			return result
+
+		case "multiply":
+			result = num1 * num2
+			return result
+
+		case "divide":
+			if num2 != 0:
+				result = num1 / num2
+				return result
+			else:
+				print("Error: Cannot divide by zero.")
+		case _:
+			print("Invalid operation selected.")

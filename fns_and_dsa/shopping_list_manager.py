a2831a7a0ea3d4b34349565a0e85c23b81024f39
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-07-23 14:50:44 +0300

    Update shopping_list_manger.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,13 +13,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item name: ")
+            item = input("Enter the item the item to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to your shopping list.")
 
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from your shopping list.")

69786f19e3f71700979195f576f6dca5bdfd3cfe
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-07-31 12:00:19 +0300

    Update test_simple_robust_division.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,25 +6,20 @@ class TestSimpleCalculator(unittest.TestCase):
 		"""Set up the SimpleCalculator instance before each test."""
 		self.calc = SimpleCalculator()
 
-	def test_addition(self):
+	def test_add(self):
 		self.assertEqual(self.calc.add(2, 3), 5)
 		self.assertEqual(self.calc.add(-1, 1), 0)
-		self.assertEqual(self.calc.add(-1, -1), -2)
-		self.assertEqual(self.calc.add(4, 1), 5)
+		self.assertEqual(self.calc.add(0, 0), 0)
 
 	def test_subtract(self):
-		"""Test the subtract method."""
-		self.assertEqual(self.calc.subtract(2, 3), -1)
-		self.assertEqual(self.calc.subtract(-1, 1), -2)
-		self.assertEqual(self.calc.subtract(-1, -1), 0)
-		self.assertEqual(self.calc.subtract(4, 1), 3)
+		self.assertEqual(self.calc.subtract(10, 5), 5)
+		self.assertEqual(self.calc.subtract(0, 5), -5)
+		self.assertEqual(self.calc.subtract(3, 3), 0)
 
 	def test_multiply(self):
-		"""Test the multiply method."""
-		self.assertEqual(self.calc.multiply(2, 3), 6)
-		self.assertEqual(self.calc.multiply(-1, 1), -1)
-		self.assertEqual(self.calc.multiply(-1, -1), 1)
-		self.assertEqual(self.calc.multiply(4, 1), 4)
+		self.assertEqual(self.calc.multiply(4, 5), 20)
+		self.assertEqual(self.calc.multiply(-2, 3), -6)
+		self.assertEqual(self.calc.multiply(0, 100), 0)
 
 	def test_divide(self):
 		"""Test the divide method."""
@@ -33,7 +28,6 @@ class TestSimpleCalculator(unittest.TestCase):
 		self.assertEqual(self.calc.divide(4, 1), 4)
 		self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333, places=5)
 		
-
 	def test_divide_by_zero(self):
 		with self.assertRaises(ZeroDivisionError):
 			self.calc.divide(10,0)

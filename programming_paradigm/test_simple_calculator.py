c393820f1ecf4d17588944966b153f9137ff5ab3
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-07-31 15:14:04 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,29 +6,29 @@ class TestSimpleCalculator(unittest.TestCase):
 		self.calc = SimpleCalculator()
 
 	def test_addition(self):
-		self.assertEqual(self.calc.add(8, 1), 9)
-		self.assertEqual(self.calc.add(2, 3), 5)
-		self.assertEqual(self.calc.add(-1, 1), 0)
-		self.assertEqual(self.calc.add(0, 0), 0)
+		self.assertEqual(self.calc.add(8,1),9)
+		self.assertEqual(self.calc.add(2,3),5)
+		self.assertEqual(self.calc.add(-1,1),0)
+		self.assertEqual(self.calc.add(0,0)0)
 
 	def test_subtract(self):
-		self.assertEqual(self.calc.subtract(4, 4), 0)
-		self.assertEqual(self.calc.subtract(10, 5), 5)
-		self.assertEqual(self.calc.subtract(0, 5), -5)
-		self.assertEqual(self.calc.subtract(3, 3), 0)
+		self.assertEqual(self.calc.subtract(4,4),0)
+		self.assertEqual(self.calc.subtract(10,5),5)
+		self.assertEqual(self.calc.subtract(0,5),-5)
+		self.assertEqual(self.calc.subtract(3,3),0)
 
 	def test_multiply(self):
-		self.assertEqual(self.calc.multiply(1, 4), 4)
-		self.assertEqual(self.calc.multiply(4, 5), 20)
-		self.assertEqual(self.calc.multiply(-2, 3), -6)
-		self.assertEqual(self.calc.multiply(0, 100), 0)
+		self.assertEqual(self.calc.multiply(1,4),4)
+		self.assertEqual(self.calc.multiply(4,5),20)
+		self.assertEqual(self.calc.multiply(-2,3),-6)
+		self.assertEqual(self.calc.multiply(0,100),0)
 
 	def test_divide(self):
-		self.assertEqual(self.calc.divide(4, 2), 2)
-		self.assertEqual(self.calc.divide(6, 2), 3)
-		self.assertEqual(self.calc.divide(-1, 1), -1)
-		self.assertEqual(self.calc.divide(4, 1), 4)
-		self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333, places=5)
+		self.assertEqual(self.calc.divide(4,2),2)
+		self.assertEqual(self.calc.divide(6,2),3)
+		self.assertEqual(self.calc.divide(-1,1),-1)
+		self.assertEqual(self.calc.divide(4,1),4)
+		self.assertAlmostEqual(self.calc.divide(7,3),2.3333333,places=5)
 		
 	def test_divide_by_zero(self):
 		with self.assertRaises(ZeroDivisionError):
